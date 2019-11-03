a3e1079953c30dedbdfd561adf525657cc3bde35
kazuma1989 <kazuma.ebina@quipper.com>
2019-11-03 09:03:26 +0900

    feat: Apps skeleton

---
+++ index.tsx
@@ -0,0 +1,26 @@
+import ReactDOM from "react-dom";
+import React from "react";
+
+function App({ style }: { style: React.CSSProperties }) {
+  return <div style={style}>app</div>;
+}
+
+const wrapper: React.CSSProperties = {
+  display: "flex",
+  flexFlow: "row nowrap",
+  height: "100%"
+};
+
+const app: React.CSSProperties = {
+  border: "solid 1px black",
+  flex: "1 1 auto"
+};
+
+ReactDOM.render(
+  <div style={wrapper}>
+    <App style={app} />
+    <App style={app} />
+    <App style={app} />
+  </div>,
+  document.getElementById("app")
+);
