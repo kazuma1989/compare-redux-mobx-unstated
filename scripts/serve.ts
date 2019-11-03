cb3c33d04672faf896cd7fae8bce5882c328779f
kazuma1989 <kazuma.ebina@quipper.com>
2019-11-03 16:36:27 +0900

    chore: Add stub API server

---
+++ scripts/serve.ts
@@ -0,0 +1,87 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+app.get("/api/notifications", (req, resp) => {
+  resp.json(stubNotifications);
+});
+
+app.get("/api/notifications/:id", (req, resp) => {
+  const notification = stubNotifications.find(n => n.id === req.params.id);
+  if (!notification) {
+    resp.sendStatus(404);
+    return;
+  }
+
+  resp.json(notification);
+});
+
+app.listen(8080, () => console.log(`Listening on port 8080`));
+
+type Notification = {
+  id: string;
+  read: boolean;
+  title: string;
+  items: {
+    type: "url" | "image" | "caption" | "text";
+    body: string;
+  }[];
+};
+
+const stubNotifications: Notification[] = [
+  {
+    id: "xxx",
+    read: false,
+    title: "The title",
+    items: [
+      {
+        type: "image",
+        body: "https://via.placeholder.com/600x200.png"
+      },
+      {
+        type: "url",
+        body: "https://via.placeholder.com/600x200.png"
+      },
+      {
+        type: "caption",
+        body: "The caption"
+      },
+      {
+        type: "text",
+        body: "Lorem ipsum"
+      }
+    ]
+  },
+  {
+    id: "yyy",
+    read: true,
+    title: "The title",
+    items: [
+      {
+        type: "caption",
+        body: "The caption"
+      },
+      {
+        type: "text",
+        body: "Lorem ipsum"
+      }
+    ]
+  },
+  {
+    id: "zzz",
+    read: false,
+    title: "The title",
+    items: [
+      {
+        type: "text",
+        body: "Lorem ipsum"
+      },
+      {
+        type: "text",
+        body: "Lorem ipsum, dolor"
+      }
+    ]
+  }
+];
